3c9996fe468570c5b09b31589b3732f9c54c0caf
Asim Ali <159928669+AsimAli-CS@users.noreply.github.com>
2024-07-08 12:29:16 +0500

    DSA changed ToDoListWithOOP.py

--- ToDoListWithOOP.py
+++ ToDoListWithOOP.py
@@ -6,64 +6,67 @@ class Item:
 
     def display_items(self):
         if self.status==True:
-            symbol1="\u2714"
+            symbol = "\u2714"
+
         else:
-            symbol1="\u0020"
-        print(f"{self.id} {self.name} {symbol1} ")
+            symbol = "\u274C"
+        print(f"{self.id} {self.name} {symbol} ")
 
 
 
 class ToDoList:
     def __init__(self):
-        self.items = []
+        self.items = {}
 
-    def add_item(self, item):
-        self.items.append(item)
+    def add_item(self, id, item):
+        if id not in self.items:
+            self.items[id] = item
+        else:
+            print(f"Item with id {id} is already listed.")
 
     def display_list(self):
-        for item in self.items:
+        for item in self.items.values():
             item.display_items()
-        print("-"*40)
-    def remeove_item(self,id):
-        for item in self.items:
-            if item.id==id:
-                self.items.remove(item)
-                break
+        print("-" * 40)
 
-    def update_item_taskName(self,id,name):
-        for item in self.items:
-            if item.id==id:
-                item.name=name
-                break
+    def remeove_item(self, id):
+        if id in self.items:
+            self.items.pop(id)
+        else:
+            print(f"Item with id {id} not found.")
 
-    def update_item_taskStatus(self,id,status):
-        for item in self.items:
-            if item.id==id:
-                item.status=status
-                break
+    def update_item(self, id, **kwargs):
+        if id in self.items:
+            if 'name' in kwargs:
+                self.items[id].name = kwargs['name']
+            if 'status' in kwargs:
+                self.items[id].status = kwargs['status']
+        else:
+            print(f"Item with id {id} not found.")
 
-def valid_input():
 
-    valid_input = False
-    while not valid_input:
+def check_user_input():
+
+    check_user_input = False
+    while not check_user_input:
         try:
             a = int(input("Enter a number between 1 to 6: "))
-            valid_input = True
+            check_user_input = True
         except ValueError:
             print("Invalid Input. ")
     return a
 
-def valid_input_id(str):
-    valid_input = False
-    while not valid_input:
+def check_id(str):
+    check_user_input = False
+    while not check_user_input:
         try:
             a = int(input(str))
-            valid_input = True
+            check_user_input = True
         except ValueError:
             print("Invalid Input.")
     return a
 
-def valid_status():
+def check_status():
     while True:
         try:
             status = int(input("Press 1 if the task is completed, press 2 if not completed: "))
@@ -83,8 +86,8 @@ if __name__ == '__main__':
 
     todo_list = ToDoList()
 
-    todo_list.add_item(task1)
-    todo_list.add_item(task2)
+    todo_list.add_item(1,task1)
+    todo_list.add_item(2,task2)
 
     print("Welcome to My ToDOList App".center(80,"-"))
 
@@ -98,21 +101,21 @@ while execute!=6:
     print("Press 6 If you want to exit")
     print("-" * 40)
 
-    select_input=valid_input()
+    selected_input=check_user_input()
     print("-" * 40)
 
-    if select_input==1:
+    if selected_input==1:
         a=0
         while(a!=-1):
 
-            id=valid_input_id("Enter the id: ")
-            print("Enter the task name: ")
+            id=check_id("Enter the id: ")
+            print("Enter the task name: ",end="")
             task = input()
             # print("Enter the Status: ")
-            status=valid_status()
+            status=check_status()
             item=Item(id,task,status)
-            todo_list.items.append(item)
-            print("Do you want to add more items y/n ")
+            todo_list.add_item(id,item)
+            print("Do you want to add more items y/n: ")
             check=input()
             a = 0 if check == "y" else -1
         print("Tasks in ToDoList")
@@ -120,31 +123,31 @@ while execute!=6:
 
 
 
-    if select_input==2:
+    if selected_input==2:
         print("Tasks in ToDoList")
         todo_list.display_list()
 
-    if select_input==3:
-        id=valid_input_id("Enter the id of the task you want to remove")
+    if selected_input==3:
+        id=check_id("Enter the id of the task you want to remove: ")
         todo_list.remeove_item(id)
         print("Tasks in ToDoList")
         todo_list.display_list()
 
-    if select_input==4:
-        id=valid_input_id("Enter the id of the task you want to update the name")
-        print("Enter the Name you have to update")
+    if selected_input==4:
+        id=check_id("Enter the id of the task you want to update the name: ")
+        print("Enter the Name you have to update: ", end="")
         name=input()
-        todo_list.update_item_taskName(id,name)
+        todo_list.update_item(id,name=name)
         print("Tasks in ToDoList")
         todo_list.display_list()
 
-    if select_input==5:
-        id=valid_input_id("Enter the id of the task you want to update the status")
-        print("Enter the status you have to update")
-        status=valid_status()
-        todo_list.update_item_taskStatus(id,status)
+    if selected_input==5:
+        id=check_id("Enter the id of the task you want to update the status: ")
+        print("Enter the status you have to update: ", end="")
+        status=check_status()
+        todo_list.update_item(id,status=status)
         print("Tasks in ToDoList")
         todo_list.display_list()
 
-    if select_input==6:
+    if selected_input==6:
         execute=6
